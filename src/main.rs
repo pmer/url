1390eaa15008845ae7b7d36ebc0b61aabd7c19de
Paul Merrill <pdm@pdm.me>
2017-03-07 00:29:25 -0800

    Add edge library

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+extern crate edge;
 extern crate rusqlite;
 extern crate time;
 
 mod db;
+
+use std::io;
+use edge::{Container, Request, Response};
+
+struct MyApp;
+impl MyApp {
+    fn hello(&self, req: &mut Request, mut res: Response) -> io::Result<()> {
+        res.content_type("text/plain");
+        res.send("Hello, world!").unwrap();
+        Ok(())
+    }
+}
 
 fn main() {
     let d = db::DB::open();
@@ -15,4 +28,8 @@ fn main() {
     for article in d.get_articles() {
         println!("Found article {:?}", article);
     }
+
+    let mut cter = Container::new(MyApp);
+    cter.get("/", MyApp::hello);
+    cter.start("0.0.0.0:3000").unwrap();
 }
